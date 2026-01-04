5555dbe8c98b681d6dd42cc40e9c11960f2fff4a
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2026-01-04 17:37:32 +0100

    [ci skip] Updated copyright header

--- src/main/java/net/raphimc/openauthmodplugin/OpenAuthModConstants.java
+++ src/main/java/net/raphimc/openauthmodplugin/OpenAuthModConstants.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of ViaProxyOpenAuthMod - https://github.com/ViaVersionAddons/ViaProxyOpenAuthMod
- * Copyright (C) 2024-2025 RK_01/RaphiMC and contributors
+ * Copyright (C) 2024-2026 RK_01/RaphiMC and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
